b7a0724586e470562186b457be6252f2dcc45cc3
moovendran <71313528+moovendran-368@users.noreply.github.com>
2024-02-15 21:56:42 +0530

    Update update_domains.sh

--- update_domains.sh
+++ update_domains.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/moovendran-368/hotstar-proxy/master/data/conf/zones.override.template | \
+curl https://raw.githubusercontent.com/moovendran-368/netflix-proxy/master/data/conf/zones.override.template | \
     awk '/^zone\s/ { if (match($2, /^"(.+)\."$/, m) != 0) print m[1]; }' > domains
 
 # to check your speed run: wget https://cachefly.cachefly.net/100mb.test
